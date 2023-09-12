a366adf7a780a19346c4b9805a87b4705ab3f4e4
nurseda-diker <nursedadiker760@gmail.com>
2023-09-13 00:23:20 +0300

    fluent validation added

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,65 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+
+            builder.RegisterType<CompanyManager>().As<ICompanyService>().SingleInstance();
+            builder.RegisterType<EfCompanyDal>().As<ICompanyDal>().SingleInstance();
+
+            builder.RegisterType<DriverManager>().As<IDriverService>().SingleInstance();
+            builder.RegisterType<EfDriverDal>().As<IDriverDal>().SingleInstance();
+
+            builder.RegisterType<MessageManager>().As<IMessageService>().SingleInstance();
+            builder.RegisterType<EfMessageDal>().As<IMessageDal>().SingleInstance();
+
+            builder.RegisterType<RequestTypeManager>().As<IRequestTypeService>().SingleInstance();
+            builder.RegisterType<EfRequestTypeDal>().As<IRequestTypeDal>().SingleInstance();
+
+            builder.RegisterType<ReservationManager>().As<IReservationService>().SingleInstance();
+            builder.RegisterType<EfReservationDal>().As<IReservationDal>().SingleInstance();
+
+            builder.RegisterType<ReviewManager>().As<IReviewService>().SingleInstance();
+            builder.RegisterType<EfReviewDal>().As<IReviewDal>().SingleInstance();
+
+
+            builder.RegisterType<TransportationTeamManager>().As<ITransportationTeamService>().SingleInstance();
+            builder.RegisterType<EfTransportationTeamDal>().As<ITransportationTeamDal>().SingleInstance();
+
+            builder.RegisterType<TransportRequestManager>().As<ITransportRequestService>().SingleInstance();
+            builder.RegisterType<EfTransportRequestDal>().As<ITransportRequestDal>().SingleInstance();
+
+            builder.RegisterType<VehicleManager>().As<IVehicleService>().SingleInstance();
+            builder.RegisterType<EfVehicleDal>().As<IVehicleDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
+
+
+
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -11,43 +14,19 @@ builder.Services.AddCors();
 
 
 
-builder.Services.AddSingleton<IUserService, UserManager>();
-builder.Services.AddSingleton<IUserDal, EfUserDal>();
-
-builder.Services.AddSingleton<ICompanyService, CompanyManager>();
-builder.Services.AddSingleton<ICompanyDal, EfCompanyDal>();
-
-builder.Services.AddSingleton<IDriverService, DriverManager>();
-builder.Services.AddSingleton<IDriverDal, EfDriverDal>();
-
-builder.Services.AddSingleton<IMessageService, MessageManager>();
-builder.Services.AddSingleton<IMessageDal, EfMessageDal>();
-
-builder.Services.AddSingleton<IRequestTypeService, RequestTypeManager>();
-builder.Services.AddSingleton<IRequestTypeDal, EfRequestTypeDal>();
-
-builder.Services.AddSingleton<IReservationService, ReservationManager>();
-builder.Services.AddSingleton<IReservationDal, EfReservationDal>();
-
-builder.Services.AddSingleton<IReviewService, ReviewManager>();
-builder.Services.AddSingleton<IReviewDal, EfReviewDal>();
-
-builder.Services.AddSingleton<ITransportationTeamService, TransportationTeamManager>();
-builder.Services.AddSingleton<ITransportationTeamDal, EfTransportationTeamDal>();
-
-builder.Services.AddSingleton<ITransportRequestService, TransportRequestManager>();
-builder.Services.AddSingleton<ITransportRequestDal, EfTransportRequestDal>();
-
-builder.Services.AddSingleton<IVehicleService, VehicleManager>();
-builder.Services.AddSingleton<IVehicleDal, EfVehicleDal>();
-
-
-
-
-
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host
+       .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+       .ConfigureContainer<ContainerBuilder>(builder =>
+       {
+           builder.RegisterModule(new AutofacBusinessModule());
+       });
+
+
+
+
 // Add services to the container.
 
 
